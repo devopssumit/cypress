e7e31e0e47220cf2df089da6f3dece8257efb51a
devopssumit <102575415+devopssumit@users.noreply.github.com>
2025-02-17 20:13:42 +0530

    push

--- cypress.config.js
+++ cypress.config.js
@@ -6,3 +6,21 @@ module.exports = {
     specPattern: 'cypress/integration/**/*.js',
   },
 };
+
+
+
+const { defineConfig } = require('cypress');
+const slackReporter = require('cypress-slack-reporter');
+
+module.exports = defineConfig({
+  e2e: {
+    supportFile: false, // Disable support file if not needed
+    baseUrl: 'https://www.google.com', // Optional: Set a base URL if needed
+    specPattern: 'cypress/integration/**/*.js',
+    pecPattern: 'cypress/integration/**/*.js',
+    setupNodeEvents(on, config) {
+      slackReporter(on, config);
+      return config;
+    },
+  },
+});
--- cypress/integration/smoke_test_spec.js
+++ cypress/integration/smoke_test_spec.js
@@ -4,5 +4,52 @@ describe('Google Smoke Test', () => {
     cy.visit('https://www.google.com');
     cy.title().should('include', 'Google');
   });
-});
 
+  it('should have a search input field', () => {
+    cy.visit('https://www.google.com');
+    cy.get('input[name="q"]').should('exist');
+  });
+
+  it('should have a Google logo', () => {
+    cy.visit('https://www.google.com');
+    cy.get('img[alt="Google"]').should('exist');
+  });
+
+  it('should have a sign-in button', () => {
+    cy.visit('https://www.google.com');
+    cy.get('a[href="https://accounts.google.com/ServiceLogin"]').should('exist');
+  });
+
+  it('should be able to search for a keyword', () => {
+    cy.visit('https://www.google.com');
+    cy.get('input[name="q"]').type('Cypress testing{enter}');
+    cy.url().should('include', 'search');
+  });
+
+  it('should show search results for a query', () => {
+    cy.visit('https://www.google.com');
+    cy.get('input[name="q"]').type('Cypress testing{enter}');
+    cy.get('#search').should('exist');
+  });
+
+  it('should load Google images', () => {
+    cy.visit('https://www.google.com/imghp');
+    cy.get('input[aria-label="Search by image"]').should('exist');
+  });
+
+  it('should show Google news', () => {
+    cy.visit('https://news.google.com');
+    cy.get('h1').contains('Top stories');
+  });
+
+  it('should have a privacy policy link', () => {
+    cy.visit('https://www.google.com');
+    cy.contains('Privacy').should('exist');
+  });
+
+  it('should load the Google homepage in less than 2 seconds', () => {
+    const start = Date.now();
+    cy.visit('https://www.google.com');
+    cy.wrap(Date.now() - start).should('be.lt', 2000);
+  });
+});
