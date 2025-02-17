bbddef691080b667f811433c16849e9a2e25c6d8
devopssumit <102575415+devopssumit@users.noreply.github.com>
2025-02-17 19:34:54 +0530

    first push

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+// cypress.config.js
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // You can add custom Node.js event listeners here if needed
+    },
+    baseUrl: 'https://www.google.com', // Optional: Set a base URL if needed
+  },
+};
+
