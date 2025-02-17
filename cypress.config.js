09226c45d4a2726ca3372240db9de57fef29b046
devopssumit <102575415+devopssumit@users.noreply.github.com>
2025-02-17 20:06:18 +0530

    push

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ module.exports = {
   e2e: {
     supportFile: false, // Disable support file if not needed
     baseUrl: 'https://www.google.com', // Optional: Set a base URL if needed
+    specPattern: 'cypress/integration/**/*.js',
   },
 };
